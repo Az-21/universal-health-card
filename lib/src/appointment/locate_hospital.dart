56ec54d8126c875bf996917353ac1284bbd553a6
Az21 <39803478+Az-21@users.noreply.github.com>
2021-04-10 12:35:33 +0530

    minified code -> reused widget

--- lib/src/appointment/locate_hospital.dart
+++ lib/src/appointment/locate_hospital.dart
@@ -2,10 +2,7 @@
 //  * Imports
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 import 'package:flutter/material.dart';
-
-// Pub Dev Imports
-import 'package:url_launcher/url_launcher.dart';
-import 'package:maps_launcher/maps_launcher.dart';
+import 'package:health_app/src/functions.dart';
 
 // ⸻⸻⸻⸻⸻⸻⸻⸻
 // * Stateless URL Launcher
@@ -13,194 +10,33 @@ import 'package:maps_launcher/maps_launcher.dart';
 class HospitalLocator extends StatelessWidget {
   const HospitalLocator({Key? key}) : super(key: key);
 
-  final String _url = 'https://www.google.com/search?q=hospital+near+me';
-
-  void _launchGoogleSearch() async => await canLaunch(_url)
-      ? await launch(_url)
-      : throw 'Could not launch $_url';
-
-  void _launchMaps() {
-    MapsLauncher.launchQuery('hospital near me');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
           SizedBox(height: 32),
-          // * Launch Map (refactor)
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              elevation: 10,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                      'assets/image/map.jpg',
-                    ),
-                  ),
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // * Title
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Icon(
-                              Icons.map,
-                              color: Colors.white,
-                              size: 32,
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              'Locate Hospital',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-
-                      SizedBox(height: 10),
-
-                      // * Subtitle
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          '\nLocate the nearby hospitals using your default map application.\n\nDefaults to Google Maps.',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-
-                      SizedBox(height: 32),
-
-                      // * Action
-                      Container(
-                        alignment: Alignment.center,
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        child: ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(
-                            onPrimary: Colors.white,
-                            primary: Colors.green,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 10),
-                            // minimumSize: Size(200, 100),
-                          ),
-                          icon: Icon(Icons.launch),
-                          label: Text('Launch'),
-                          onPressed: () {
-                            _launchMaps();
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+          // * Launch map
+          ImageFunctionCard(
+            title: 'Locate Hospital',
+            subtitle:
+                '\nLocate the nearby hospitals using your default map application.\n\nDefaults to Google Maps.',
+            icon: Icons.map,
+            imagePath: 'assets/image/map.jpg',
+            url: 'hospital near me',
+            isMap: true,
           ),
 
           SizedBox(height: 32),
 
-          // * Launch Search (refactor)
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              elevation: 10,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                      'assets/image/search.jpg',
-                    ),
-                  ),
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // * Title
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Icon(
-                              Icons.map,
-                              color: Colors.white,
-                              size: 32,
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              'Search Hospital',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-
-                      SizedBox(height: 10),
-
-                      // * Subtitle
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          '\nSearch hospitals near you using Google.',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-
-                      SizedBox(height: 32),
-
-                      // * Action
-                      Container(
-                        alignment: Alignment.center,
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        child: ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(
-                            onPrimary: Colors.white,
-                            primary: Colors.green,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 10),
-                            // minimumSize: Size(200, 100),
-                          ),
-                          icon: Icon(Icons.launch),
-                          label: Text('Launch'),
-                          onPressed: () {
-                            _launchGoogleSearch();
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+          // * Launch search
+          ImageFunctionCard(
+            title: 'Search Hospital',
+            subtitle: '\nSearch hospitals near you using Google',
+            icon: Icons.search,
+            imagePath: 'assets/image/search.jpg',
+            url: 'https://www.google.com/search?q=hospital+near+me',
+            isMap: false,
           ),
         ],
       ),
--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 
 // ⸻⸻⸻⸻⸻⸻⸻⸻
 // * Function: Print Day | Date
@@ -32,4 +34,124 @@ void infoSnackbar(String title, String subtitle, bool success) {
     isDismissible: true,
     duration: Duration(seconds: 5),
   );
+}
+
+// ⸻⸻⸻⸻⸻⸻⸻⸻
+// * Reused widget: image card launcher
+// ⸻⸻⸻⸻⸻⸻⸻⸻
+class ImageFunctionCard extends StatelessWidget {
+  const ImageFunctionCard({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.imagePath,
+    required this.url,
+    required this.isMap,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final String imagePath;
+  final String url;
+  final bool isMap; // 1 = map, 0 = search
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        elevation: 10,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                imagePath,
+              ),
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // * Title
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Icon(
+                        icon,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // * Subtitle
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    subtitle,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+
+                SizedBox(height: 32),
+
+                // * Action
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.white,
+                      primary: Colors.green,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                      // minimumSize: Size(200, 100),
+                    ),
+                    icon: Icon(Icons.launch),
+                    label: Text('Launch'),
+                    onPressed: () {
+                      _launchURL(url, isMap);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _launchURL(String url, bool isMap) {
+    isMap ? _launchMaps() : _launchGoogleSearch();
+  }
+
+  // * Function: launch maps
+  void _launchMaps() => MapsLauncher.launchQuery(url);
+
+  // * Function: launch search
+  void _launchGoogleSearch() async =>
+      await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
 }
