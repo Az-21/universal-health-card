2b741543a55540ce37df88ffe8d667ca2c7d42bd
DhruvK1999 <dkanthaliya1999@gmail.com>
2021-06-03 09:39:43 +0530

    hardcoded verification

---
+++ hardware/arduino.c
@@ -0,0 +1,58 @@
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+int rfidtag['123456789','123456654','1234586723']               #Added the authorized cards here
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show RFID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  
+  for(i=0;i=100;i++)
+  {
+  if (content.substring(1) == rfidtagno[i]) //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Card Recognized !");
+    Serial.println();
+    delay(2000);
+  }
+  else
+  {
+  continue;	 
+  }
+  }
+   } 
