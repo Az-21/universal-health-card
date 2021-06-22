1790070a72f67beea52326de1495be076629352f
Ash_Bajit13 <48647015+Ashbajit@users.noreply.github.com>
2021-06-23 00:01:29 +0530

    formatted

--- hardware/bluetooth.c
+++ hardware/bluetooth.c
@@ -2,33 +2,36 @@
 
 String text;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  while(Serial.available())
+  while (Serial.available())
   {
     delay(10);
     char c = Serial.read();
     text += c;
   }
-  if(text.length()>0)
+  if (text.length() > 0)
   {
     Serial.println(text);
-    
-    if(text == "on")
+
+    if (text == "on")
     {
-      digitalWrite(13,HIGH);
+      digitalWrite(13, HIGH);
       Serial.println("PORT is on");
     }
-    if(text == "off")
+    if (text == "off")
     {
-      digitalWrite(13, LOW); 
+      digitalWrite(13, LOW);
       Serial.println("PORT is off");
     }
-    text = ""; }
+    text = "";
+  }
 }
